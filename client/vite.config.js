1fb25b48d97cad2b06f7537384f5fcb9d9728ebc
เฉินหม่า <117138644+Lskram@users.noreply.github.com>
2025-11-10 23:32:22 +0700

    02:22:12

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 3000,
     watch: {
 
@@ -13,7 +13,8 @@ export default defineConfig({
     },
     hmr: {
 
-      clientPort: 3000
+      clientPort: 3000,
+      // clientPort: 8880
 
     }
   }
